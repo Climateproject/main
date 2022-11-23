c42517a091ed3dde10c65dea72030146c7939123
inkahirsila <inka.hirsila@gmail.com>
2022-11-23 10:02:59 +0200

    muokattu

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import {bar, line, pie} from 'react-chartjs-2';
+import {chart as chartJS} from "chart.js/auto";
 import {Container} from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
 import { Navbar } from 'react-bootstrap';
@@ -17,27 +19,38 @@ function App() {
           <Container>
              {/* Lisätään hampparimenu */}
             <Navbar.Toggle aria-controls='basic-navbar-nav'></Navbar.Toggle>
-            {/* Navi piilotetaan kapeammassa näytössö (vain hamppari-ikoni näkyy) */}
             <Navbar.Collapse id='basic-navbar-nav'>
                  {/* onSelect asettaa menussa valitun itemin eventKeyn */}
               <Nav className="me-auto" onSelect={sel => setSelection(sel)}>
-                  <Nav.Link eventKey={"kaikilla on hauskaa!"}>Tsemppi</Nav.Link>
-                  <NavDropdown title="Kiinnostaako?" id="basic-nav-dropdown">
-                    <NavDropdown.Item eventKey={"jjee moti satasessa"}>Samuli</NavDropdown.Item>
-                    <NavDropdown.Item eventKey={"wwuhuu"}>Meri</NavDropdown.Item>
-                    <NavDropdown.Divider /> {/* Tää erittelee meidät*/}
-                    <NavDropdown.Item eventKey={"koodaus on kivaa"}>Inka</NavDropdown.Item>
-                    <NavDropdown.Item eventKey={":--)"}>Juuso</NavDropdown.Item>
+                  <Nav.Link eventKey={"kaikilla on hauskaa!"}>Etusivu</Nav.Link>
+                  <NavDropdown title="Valikko" id="basic-nav-dropdown">
+                    <NavDropdown.Item eventKey={1}>Samuli</NavDropdown.Item>
+                    <NavDropdown.Divider /> 
+                    <NavDropdown.Item eventKey={2}>Meri</NavDropdown.Item>
+                    <NavDropdown.Divider /> 
+                    <NavDropdown.Item eventKey={3}>Inka</NavDropdown.Item>
+                    <NavDropdown.Divider /> 
+                    <NavDropdown.Item eventKey={4}>Juuso</NavDropdown.Item>
                   </NavDropdown>
                 </Nav>
             </Navbar.Collapse>
           </Container>
       </Navbar>
     
-      <h1>Mahtavaa jee {selection}</h1>
+      {selection == 2 ? <Chart1/> : <h1>jok muu sivus</h1>}
+     
 
     </div>
   );
 }
 
+function Chart1()
+{
+    return (
+      <div> style={{ display}}
+        Tässä o chart1
+      </div>
+    )
+}
 export default App;
+
