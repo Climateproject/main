3dd62d84e22c9f377e774ed80cc8fc10971bb91e
KuusamonZyzz <potteson@hotmail.com>
2022-12-01 10:30:08 +0200

    turha

--- climate/src/main/java/com/db/climate/ClimateApplication.java
+++ climate/src/main/java/com/db/climate/ClimateApplication.java
@@ -8,7 +8,6 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClimateApplication {
