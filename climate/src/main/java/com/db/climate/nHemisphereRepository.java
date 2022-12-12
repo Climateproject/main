760832d697deb50ecf2f07dbb30a8d054af638c8
Samulikoulu <99029996+Samulikoulu@users.noreply.github.com>
2022-12-12 12:00:41 +0200

    Delete nHemisphereRepository.java

--- climate/src/main/java/com/db/climate/nHemisphereRepository.java
+++
@@ -1,8 +0,0 @@
-package com.db.climate;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface NhemisphereRepository extends JpaRepository<Nhemisphere, Integer> {
-    
-}
