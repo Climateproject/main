00610359feb0595d859b2de228f42732b4d9fd9b
Samulikoulu <t1casa00@students.oamk.fi>
2022-12-12 12:06:09 +0200

    Testitesti

---
+++ climate/src/main/java/com/db/climate/NhemisphereRepository.java
@@ -0,0 +1,8 @@
+package com.db.climate;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface NhemisphereRepository extends JpaRepository<Nhemisphere, Integer> {
+    
+}
